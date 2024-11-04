f47f61cb70c8f2d2d0149288a4ec19612607d900
Vadim Nikitin <vchivas18years@gmail.com>
2024-11-04 12:53:00 +0400

    check

--- src/components/AuthRedirect/AuthRedirect.tsx
+++ src/components/AuthRedirect/AuthRedirect.tsx
@@ -4,6 +4,7 @@ import { useAppNavigate, useTelegram } from '@/hooks';
 export const AuthRedirect = () => {
   const { id } = useTelegram();
   const { goToStartPage, goToAuthPage } = useAppNavigate();
+  console.log(id);
 
   useEffect(() => {
     if (id === '1132630506' || id === '280676256') {
