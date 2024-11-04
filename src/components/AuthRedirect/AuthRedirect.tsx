2851efdf45c29fdef4d6e77ce8113e22860783a7
Vadim Nikitin <vchivas18years@gmail.com>
2024-11-04 13:18:55 +0400

    check

--- src/components/AuthRedirect/AuthRedirect.tsx
+++ src/components/AuthRedirect/AuthRedirect.tsx
@@ -1,18 +1,25 @@
 import { useEffect } from 'react';
 import { useAppNavigate, useTelegram } from '@/hooks';
 
-export const AuthRedirect = () => {
+export const AuthRedirect = ({ children }: any) => {
   const { id } = useTelegram();
   const { goToStartPage, goToAuthPage } = useAppNavigate();
 
+  if (!id) {
+    return <div>Loading...</div>;
+  }
+
   useEffect(() => {
-    console.log('USE EFEEct', id);
-    if (id === '1132630506' || id === '280676256') {
-      goToStartPage(1);
-    } else {
-      goToAuthPage();
+    if (id) {
+      console.log('USE EFFECT', id);
+      if (id === '1132630506' || id === '280676256') {
+        goToStartPage(1);
+        return;
+      } else {
+        goToAuthPage();
+      }
     }
   }, [id, goToStartPage, goToAuthPage]);
 
-  return <div>Redirecting...</div>;
+  return children;
 };
--- src/router.tsx
+++ src/router.tsx
@@ -14,21 +14,21 @@ import { AuthRedirect } from '@/components';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-      <Route index path="/" element={<AuthRedirect />} errorElement={<ErrorPage />} />
-      <Route index path="/auth" element={<AuthPage />} errorElement={<ErrorPage />} />
-      <Route index path="/:company_id" element={<MonitoringPage />} errorElement={<ErrorPage />} />
-      <Route
-        index
-        path="/:company_id/doughnutcharts"
-        element={<DoughnutChartsPage />}
-        errorElement={<ErrorPage />}
-      />
-      <Route
-        index
-        path="/:company_id/doughnutcharts/:typelinecharts/linecharts"
-        element={<LineChartPage />}
-        errorElement={<ErrorPage />}
-      />
+      <Route>
+        <Route index path="/" element={<AuthRedirect />} errorElement={<ErrorPage />} />
+        <Route path="/auth" element={<AuthPage />} errorElement={<ErrorPage />} />
+        <Route path="/:company_id" element={<MonitoringPage />} errorElement={<ErrorPage />} />
+        <Route
+          path="/:company_id/doughnutcharts"
+          element={<DoughnutChartsPage />}
+          errorElement={<ErrorPage />}
+        />
+        <Route
+          path="/:company_id/doughnutcharts/:typelinecharts/linecharts"
+          element={<LineChartPage />}
+          errorElement={<ErrorPage />}
+        />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </>
   )
