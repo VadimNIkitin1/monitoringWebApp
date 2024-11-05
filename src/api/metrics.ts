30f92f1f09570a1f754dc94f5b605c878136ff1b
Vadim Nikitin <vchivas18years@gmail.com>
2024-11-05 21:42:43 +0400

    fix

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://web-app-monitoring.bigdigital.pro/api/v1/metrics';
+// axios.defaults.baseURL = 'http://localhost:8080';
 
 export async function getServerList() {
   try {
